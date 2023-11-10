08b2aecf5b7d669e9cb7e109a95915baf07c1889
amirabayoumi <149677044+amirabayoumi@users.noreply.github.com>
2023-11-10 10:15:54 +0100

    run format

--- src/main.js
+++ src/main.js
@@ -2,91 +2,89 @@
 //first give difintaion to elements in html (declare the variable )
 
 const display = document.querySelector('#input');
-const buttons = document.querySelectorAll('.num-key, .operation-key, .percent-key');
+const buttons = document.querySelectorAll(
+    '.num-key, .operation-key, .percent-key'
+);
 
 let currentInput = '';
 let previousInput = '';
 let operator = null;
 
-
 // give click option to all buttons and give the condition (how it will work) for operator keys
 
-buttons.forEach(function(button) {
-  button.addEventListener('click', function(e) 
-  {
-    const buttonValue = button.value;
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (e) {
+        const buttonValue = button.value;
 
-    //reset button
-    if (buttonValue == 'Reset') {
-      currentInput = '';
-      previousInput = '';
-      operator = null;
+        //reset button
+        if (buttonValue == 'Reset') {
+            currentInput = '';
+            previousInput = '';
+            operator = null;
 
-    //delete button 
-    } else if (buttonValue == 'DEL') {
-      currentInput = currentInput.slice(0, -1);
+            //delete button
+        } else if (buttonValue == 'DEL') {
+            currentInput = currentInput.slice(0, -1);
 
-    //calculate button
-    } else if (buttonValue == '=') {
-      if (currentInput && previousInput && operator) {
-        currentInput = operate(previousInput, currentInput, operator);
-        previousInput = '';
-        operator = null;
-      }
-    // percentage button 
-    } else if (buttonValue == '%') {
-      currentInput = calculatePercentage(currentInput);
+            //calculate button
+        } else if (buttonValue == '=') {
+            if (currentInput && previousInput && operator) {
+                currentInput = operate(previousInput, currentInput, operator);
+                previousInput = '';
+                operator = null;
+            }
+            // percentage button
+        } else if (buttonValue == '%') {
+            currentInput = calculatePercentage(currentInput);
 
-    // all operators buttons
-    } else if (isOperator(buttonValue)) {
-      if (currentInput && previousInput) {
-        previousInput = operate(previousInput, currentInput, operator);
-        operator = buttonValue;
-        currentInput = '';
-      } else if (currentInput) {
-        previousInput = currentInput;
-        operator = buttonValue;
-        currentInput = '';
-      }
+            // all operators buttons
+        } else if (isOperator(buttonValue)) {
+            if (currentInput && previousInput) {
+                previousInput = operate(previousInput, currentInput, operator);
+                operator = buttonValue;
+                currentInput = '';
+            } else if (currentInput) {
+                previousInput = currentInput;
+                operator = buttonValue;
+                currentInput = '';
+            }
 
-    // adding new value in right side of existing value
-    } else {
-      currentInput += buttonValue;
-    }
-    // what is on display either curnt/perv/or zero
-    display.value = currentInput || previousInput || '0';
-  });
+            // adding new value in right side of existing value
+        } else {
+            currentInput += buttonValue;
+        }
+        // what is on display either curnt/perv/or zero
+        display.value = currentInput || previousInput || '0';
+    });
 });
 
-
-//operations 
+//operations
 function operate(a, b, op) {
-  a = parseFloat(a);
-  b = parseFloat(b);
-  switch (op) {
-    case '+':
-      return (a + b).toString();
-    case '-':
-      return (a - b).toString();
-    case '*':
-      return (a * b).toString();
-    case '/':
-      return (a / b).toString();
-    default:
-      return "0";
-  }
+    a = parseFloat(a);
+    b = parseFloat(b);
+    switch (op) {
+        case '+':
+            return (a + b).toString();
+        case '-':
+            return (a - b).toString();
+        case '*':
+            return (a * b).toString();
+        case '/':
+            return (a / b).toString();
+        default:
+            return '0';
+    }
 }
 function isOperator(value) {
-  return ['+', '-', '*', '/'].includes(value);
+    return ['+', '-', '*', '/'].includes(value);
 }
 
-// the % button directly send the result value to display without click calculate button 
+// the % button directly send the result value to display without click calculate button
 function calculatePercentage(value) {
-  const numericValue = parseFloat(value);
-  if (!isNaN(numericValue)) {
-    return (numericValue / 100).toString();
-  } else {
-    return '0';
-  }
+    const numericValue = parseFloat(value);
+    if (!isNaN(numericValue)) {
+        return (numericValue / 100).toString();
+    } else {
+        return '0';
+    }
 }
-
